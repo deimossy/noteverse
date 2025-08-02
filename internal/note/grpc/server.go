b983a5d04aa98d7e5ef61b454e429abbe0aed156
deimossy <deimossy@student.21-school.ru>
2025-08-03 00:03:12 +0500

    finished checking work of grpc server

--- internal/note/grpc/server.go
+++ internal/note/grpc/server.go
@@ -2,13 +2,25 @@ package grpc
 
 import (
 	"context"
+	"time"
 
 	pb "github.com/deimossy/noteverse/api/proto"
-
 )
 
 type NoteServiceServer struct {
 	pb.UnimplementedNoteServiceServer
 }
 
-func (s *NoteServiceServer) CreateNote(ctx context.Context)
+func (s *NoteServiceServer) CreateNote(
+	ctx context.Context,
+	req *pb.CreateNoteRequest) (*pb.CreateNoteResponse, error) {
+	note := &pb.Note{
+		Id:        "generated-unique-id",
+		UserId:    req.UserId,
+		Content:   req.Content,
+		CreatedAt: time.Now().Unix(),
+		UpdatedAt: time.Now().Unix(),
+	}
+
+	return &pb.CreateNoteResponse{Note: note}, nil
+}
